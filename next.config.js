7ffd32680ac9b2327afb291df743e8eea6cb8969
ainazarov <ainazarov@tsconsulting.com>
2021-07-12 22:05:18 +0300

    feature/add-button

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  reactStrictMode: true,
-}
+	webpack5: false,
+	images: {
+		domains: ['courses-top.ru']
+	},
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			issuer: {
+				test: /\.(js|ts)x?$/,
+			},
+			use: ['@svgr/webpack'],
+		});
+
+		return config;
+	},
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { Button } from "../components";
 export default function Home(): JSX.Element {
   return <div>
       <Htag tag='h1'>Test</Htag>
-      <Button appearance='primary'>Кнопка</Button>
-      <Button appearance='ghost'>Кнопка</Button>
+      <Button appearance='primary' arrow='right'>Кнопка</Button>
+      <Button appearance='ghost' arrow='down'>Кнопка</Button>
     </div>;
 }
