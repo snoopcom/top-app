6789589827c420eb4666552004c11799b605eb6a
ainazarov <ainazarov@tsconsulting.com>
2021-07-13 00:30:23 +0300

    feature/add-p

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,6 +1,6 @@
-import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 import ArrowIcon from './arrow.svg';
+import styles from './Button.module.css';
 import cn from 'classnames';
 
 export const Button = ({ appearance, arrow = 'none', children, className, ...props }: ButtonProps): JSX.Element => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
-import { Htag } from "../components";
-import { Button } from "../components";
+import { Htag } from '../components';
+import { Button } from '../components';
+import { P } from '../components';
 
 export default function Home(): JSX.Element {
   return <div>
       <Htag tag='h1'>Test</Htag>
       <Button appearance='primary' arrow='right'>Кнопка</Button>
       <Button appearance='ghost' arrow='down'>Кнопка</Button>
+      <P>fesfsef</P>
+      <P size='s'>fesfsef</P>
     </div>;
 }
