657e9a445b8b3737705e0adec5ec0e515a259b04
ainazarov <ainazarov@tsconsulting.com>
2021-07-12 16:21:30 +0300

    feature/add-button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
 import { Htag } from "../components";
+import { Button } from "../components";
 
 export default function Home(): JSX.Element {
-  return <div><Htag tag='h1'>Test</Htag></div>;
+  return <div>
+      <Htag tag='h1'>Test</Htag>
+      <Button appearance='primary'>Кнопка</Button>
+      <Button appearance='ghost'>Кнопка</Button>
+    </div>;
 }
